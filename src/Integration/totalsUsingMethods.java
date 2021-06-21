88f5fe02e1b1a62c8eb7ce6d6111ba49972fee75
Justinrubio <84459485+Justinrubio@users.noreply.github.com>
2021-06-21 14:09:22 -0400

    Add files via upload

---
+++ src/Integration/totalsUsingMethods.java
@@ -0,0 +1,26 @@
+package Integration;
+
+//Justin Rubio
+
+
+
+import java.util.Scanner;
+
+public class totalsUsingMethods {
+
+	// arguments are located in the parenthesis here
+	public static double totalIncomeFromHours(double totalHours, double hourlyWage) {
+		return totalHours * hourlyWage;
+	}
+
+	
+}
+
+/*
+char aChar = 'v';
+int charValue = (int)aChar;
+int shift = 5;
+charValue += shift;
+System.out.println((char)charValue);
+
+*/
